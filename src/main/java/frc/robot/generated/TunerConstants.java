042fa46f4fca501cb3057c8fe3f1a174cce84ee7
Austin L <austinledin28@icloud.com>
2026-04-11 19:45:42 -0500

    change gear ratio

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -74,13 +74,13 @@ public class TunerConstants {
 
     // Theoretical free speed (m/s) at 12 V applied output;
     // This needs to be tuned to your individual robot
-    public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(4.57);
+    public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(5.334);
 
     // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
     // This may need to be tuned to your individual robot
     private static final double kCoupleRatio = 0;
 
-    private static final double kDriveGearRatio = 6.75;
+    private static final double kDriveGearRatio = 6;
     private static final double kSteerGearRatio = 24.1;
     private static final Distance kWheelRadius = Inches.of(2);
 
