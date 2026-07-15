bee271e5f7ec8cbc1d6b24f43fc7ffde170394ee
Luke2029 <luke.wroblewski99@gmail.com>
2026-07-14 20:48:07 -0500

    Change current limits for feeder

--- src/main/java/frc/robot/subsystems/feeder/FeederIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/feeder/FeederIOTalonFX.java
@@ -51,9 +51,9 @@ public class FeederIOTalonFX implements FeederIO {
 
     TalonFXConfiguration configFollower = new TalonFXConfiguration();
     configFollower.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-    configFollower.CurrentLimits.StatorCurrentLimit = 120.0;
+    configFollower.CurrentLimits.StatorCurrentLimit = 60.0;
     configFollower.CurrentLimits.StatorCurrentLimitEnable = true;
-    configFollower.CurrentLimits.SupplyCurrentLimit = 60.0;
+    configFollower.CurrentLimits.SupplyCurrentLimit = 20.0;
     configFollower.CurrentLimits.SupplyCurrentLimitEnable = true;
     configFollower.Voltage.PeakForwardVoltage = 12.0;
     configFollower.Voltage.PeakReverseVoltage = -12.0;
