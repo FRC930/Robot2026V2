6eb5fe3281d7864e29a9bf75635d0a7f05fd0951
Austin L <austinledin28@icloud.com>
2026-07-17 10:17:04 -0500

    Floor tuning

--- src/main/java/frc/robot/subsystems/feeder/FeederIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/feeder/FeederIOTalonFX.java
@@ -40,7 +40,7 @@ public class FeederIOTalonFX implements FeederIO {
     configFeeder.MotorOutput.NeutralMode = NeutralModeValue.Coast;
     configFeeder.CurrentLimits.StatorCurrentLimit = 80.0;
     configFeeder.CurrentLimits.StatorCurrentLimitEnable = true;
-    configFeeder.CurrentLimits.SupplyCurrentLimit = 20.0;
+    configFeeder.CurrentLimits.SupplyCurrentLimit = 25.0;
     configFeeder.CurrentLimits.SupplyCurrentLimitEnable = true;
     configFeeder.Voltage.PeakForwardVoltage = 12.0;
     configFeeder.Voltage.PeakReverseVoltage = -12.0;
@@ -53,7 +53,7 @@ public class FeederIOTalonFX implements FeederIO {
     configFollower.MotorOutput.NeutralMode = NeutralModeValue.Coast;
     configFollower.CurrentLimits.StatorCurrentLimit = 80.0;
     configFollower.CurrentLimits.StatorCurrentLimitEnable = true;
-    configFollower.CurrentLimits.SupplyCurrentLimit = 20.0;
+    configFollower.CurrentLimits.SupplyCurrentLimit = 25.0;
     configFollower.CurrentLimits.SupplyCurrentLimitEnable = true;
     configFollower.Voltage.PeakForwardVoltage = 12.0;
     configFollower.Voltage.PeakReverseVoltage = -12.0;
